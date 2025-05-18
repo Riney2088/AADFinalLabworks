610c3b7417f15a4c9b7bb871fe7ae8635e15b30a
Riney2088 <mstrineykhatun691@gmail.com>
2025-05-18 19:56:20 +0600

    this is

---
+++ DFS/src/DFS/DFS.java
@@ -0,0 +1,22 @@
+package DFS;
+import java.util.LinkedList;
+import java.util.Queue;
+public class DFS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Graph graph =new Graph(7);
+         graph.addEdge(0, 1);
+         graph.addEdge(0, 2);
+         graph.addEdge(1, 3);
+         graph.addEdge(1, 4);
+         graph.addEdge(2, 5);
+         graph.addEdge(2, 6);
+
+         System.out.println("Depth-First Search (DFS) traversal starting from vertex 0:");
+         graph.DFS(0);
+
+         
+	}
+
+}
